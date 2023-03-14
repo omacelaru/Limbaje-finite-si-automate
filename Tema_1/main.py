e5bb995b7c92445a59295b85ce17cce691228bb4
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-03-14 18:51:10 +0200

    convert function in class

--- Tema_1/main.py
+++ Tema_1/main.py
@@ -1,5 +1,5 @@
 class graph:
-    def __init__(self, name_file="input1.in"):
+    def __init__(self, name_file):
         input_text = []
         with open(name_file) as f:
             self.start_node = f.readline().strip().lower()  # starea initiala
@@ -15,7 +15,8 @@ class graph:
         self.alfabet = sorted(list({x[1] for x in input_text}))
 
         # formarea functiei delta
-        self.matrix = [[0 for x in range(len(self.alfabet))] for y in range(len(self.states))]
+        self.matrix = [[0 for x in range(len(self.alfabet) + 1)] for y in range(len(self.states) + 1)]
+
         for line in input_text:
             i = int(line[0][-1])
             j = int(line[1]) if line[1].isdigit() else ord(line[1]) - ord('a')
@@ -23,24 +24,26 @@ class graph:
 
     def verify_word(self, word):
         path = []
+        self.start = self.start_node
         for c in word:
-            path.append(self.start_node)  # adaugarea componentei in drum
+            path.append(self.start)  # adaugarea componentei in drum
 
-            i = int(self.start_node[-1])  #
+            if self.start_node[-1] != 0:
+                i = int(self.start_node[-1])
             j = int(c) if c.isdigit() else ord(c) - ord('a')  # parcurgerea functiei delta
-            self.start_node = self.matrix[i][j]  #
+            self.start = self.matrix[i][j]  #
             if self.start_node == 0:  # verificarea unei erori de citire a datelor
                 print("Input gresit")
                 return False, []
 
-        path.append(self.start_node)  # adaugarea ultimei stari in drum
+        path.append(self.start)  # adaugarea ultimei stari in drum
 
-        if self.start_node in self.final_nodes:
+        if self.start in self.final_nodes:
             return True, path
         return False, []
 
-name_file_input = "input2.in"
-name_file_test = "test2.in"
+name_file_input = "input3.in"
+name_file_test = "test3.in"
 
 graph = graph(name_file_input)
 
